fe3d3a60674aad17b03b130140ed3ce02a0d6434
phuc.nguyen <phuc.nguyen@redex.eco>
2025-06-17 10:50:47 +0700

    Initial commit

--- backend/src/main/java/com/jb/auth/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.jb.auth.model;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- backend/src/main/java/com/jb/auth/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.jb.auth.service;
-
-import com.jb.auth.model.User;
-
-public interface UserService {
-    User getUserByEmail(String email);
-}
